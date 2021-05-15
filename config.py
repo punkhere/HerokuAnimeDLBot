80250163fe0c1256f3fc062a7fac194d473498db
RedAura <79897294+Red-Aura@users.noreply.github.com>
2021-05-15 18:53:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,10 @@
 # Encoding = 'utf-8'
 # Licensed under MIT License
 # Special Thanks for gogoanime
+import os
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = "YOUR_TELEGRAM_API_ID"
-api_hash = "YOUR_TELEGRAM_API_HASH"
-bot_token = "YOUR_BOT_TOKEN"
+api_id = os.environ.get("api_id", None)
+api_hash = os.environ.get("api_hash", None)
+bot_token = os.environ.get("bot_token", None)
